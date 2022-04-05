37c59403ab95ae0f45e6bac62e53ceee78e2e082
Shaded <72438879+ItsShaded@users.noreply.github.com>
2022-04-05 10:37:02 +0530

    Update ready.js

--- Discord-V14-Handler-main/events/ready.js
+++ Discord-V14-Handler-main/events/ready.js
@@ -3,7 +3,7 @@ const client = require("../index");
 client.on("ready", async () => {
   console.log(`${client.user.username} Is Online`);
   client.user.setActivity({
-    name: `@${client.user.username} /help`,
+    name: `@${client.user.username} /help | Code by why#4148 [Shaded]`,
     type: "PLAYING",
   });
 });
