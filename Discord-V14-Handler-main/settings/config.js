63939c1762abd5b18c364cf1316b14f16684f7f0
Shaded <72438879+ItsShaded@users.noreply.github.com>
2022-04-05 10:34:05 +0530

    Update config.js

--- Discord-V14-Handler-main/settings/config.js
+++ Discord-V14-Handler-main/settings/config.js
@@ -5,7 +5,7 @@ module.exports = {
   embed: {
     color: "#2f3136",
     wrongcolor: "#2f3136",
-    footertext: "Coded By Kabir Singh | Tech Boy Development",
+    footertext: "Code by why#4148 [Shaded]",
     footericon:
       "https://images-ext-1.discordapp.net/external/C2xH2P5W4KM4F6wPxElhlzyVrZ5bjWl9_TPT-X_3hfs/%3Fsize%3D512/https/cdn.discordapp.com/avatars/894158815166095390/b3963f5de220e55d185c5b9d15648a82.webp?width=424&height=424",
   },
